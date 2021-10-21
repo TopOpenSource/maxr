d762dac4b037dc82bd14a2e747c87083430d0208
cheng <807992213@qq.com>
2021-10-21 18:57:15 +0800

    1

--- Test.py
+++ Test.py
@@ -1,10 +1,10 @@
 import os
-
+import numpy as np
 import pydicom
 import matplotlib.pyplot as plt
 from utils.DcmUtil import DcmUtil
 
-DcmUtil.test()
+# DcmUtil.test()
 
 '''
 1 设置患者目录
@@ -16,34 +16,35 @@ DcmUtil.test()
 
 '''
 
-#患者目录
+# 患者目录
 patientPath = "D:\\test_data\\test_data\\0134606885310576"
-
 
 patientCTPath = patientPath + "\\CT"
 patientCBCTPath = patientPath + "\\CBCT"
 patientSCTPath = patientPath + "\\SCT"
 
-
 for root, dirs, files in os.walk(patientCTPath):
-
     for file in files:
-        #获取文件名
+        # 获取文件名
         fileName = file.split(".dcm")[0]
-        #根据文件名获取 CBCT CT SCT对应的文件
+        # 根据文件名获取 CBCT CT SCT对应的文件
         fileCBCT = patientCBCTPath + "\\I" + fileName
         fileCT = patientCTPath + "\\" + fileName + ".dcm"
         fileSCT = patientSCTPath + "\\" + fileName + ".dcm"
 
-        imageCBCT = pydicom.dcmread(fileCBCT).pixel_array
-        imageCT = pydicom.dcmread(fileCT).pixel_array
-        imageSCT = pydicom.dcmread(fileSCT).pixel_array
+        image_CBCT = pydicom.dcmread(fileCBCT).pixel_array
+        image_CT = pydicom.dcmread(fileCT).pixel_array
+        image_SCT = pydicom.dcmread(fileSCT).pixel_array
 
+        mae_CBCT = DcmUtil.mae(image_CT, image_CBCT)
+        mae_SCT = DcmUtil.mae(image_CT, image_SCT)
 
-'''
-dcm = pydicom.dcmread("D:\\test_data\\test_data\\0134606885310576\\CT\\1.dcm")
-img_arr = dcm.pixel_array
+        mse_CBCT = DcmUtil.mse(image_CT, image_CBCT)
+        mse_SCT = DcmUtil.mse(image_CT, image_SCT)
 
-dcm1 = pydicom.dcmread("D:\\test_data\\test_data\\0134606885310576\\CBCT\\I0")
-img_arr1=dcm1.pixel_array
-'''
+        rmse_CBCT = DcmUtil.rmse(image_CT, image_CBCT)
+        rmse_SCT = DcmUtil.rmse(image_CT, image_SCT)
+
+        psnr_CBCT = DcmUtil.psnr(image_CT, image_CBCT, 512)
+        psnr_SCT = DcmUtil.psnr(image_CT, image_SCT, 512)
+
--- utils/DcmUtil.py
+++ utils/DcmUtil.py
@@ -1,4 +1,26 @@
+import numpy as np
+
+
 class DcmUtil:
+
     @staticmethod
-    def test():
-        print("hello")
+    def psnr(img1, img2, pixMax):
+        mse = np.mean((img1 - img2) ** 2)
+        if mse == 0:
+            return 100
+        return 10 * np.log10(pixMax * pixMax / mse)
+
+    @staticmethod
+    def mse(img1, img2):
+        mse = np.mean((img1 - img2) ** 2)
+        return mse
+
+    @staticmethod
+    def rmse(img1, img2):
+        mse = np.mean((img1 - img2) ** 2)
+        return np.sqrt(mse)
+
+    @staticmethod
+    def mae(img1, img2):
+        mae = np.mean(np.abs(img1 - img2))
+        return mae
