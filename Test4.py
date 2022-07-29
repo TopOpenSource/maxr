603dceec538313a832731cd823b776dae4aa5827
cheng <chengang@sdas.org>
2022-07-29 09:39:27 +0800

    图像相减

--- Test4.py
+++ Test4.py
@@ -14,11 +14,13 @@ def copPatientPath(path, patientId):
 
 
     # 按照方向排序切片
+
     slices_CT = DcmUtil.sortSlices(patientCTPath)
     slices_CBCT = DcmUtil.sortSlices(patientCBCTPath)
     slices_SCT = DcmUtil.sortSlices(patientSCTPath)
 
-    for i in range(0, 10):
+    for i in range(0,len(slices_CT)):
+
         image_CBCT = DcmUtil.get_pixels_hu(slices_CBCT[i])
         image_CT = DcmUtil.get_pixels_hu(slices_CT[i])
         image_SCT = DcmUtil.get_pixels_hu(slices_SCT[i])
@@ -26,15 +28,19 @@ def copPatientPath(path, patientId):
         # 计算
         index, r_CBCT, r_SCT = DcmUtil.reduce_img(i, image_CBCT, image_CT, image_SCT)
 
+        saveImage(r_CBCT,patientCBCTPath+"_reduct\\"+str(i)+".png")
+        saveImage(r_SCT, patientSCTPath + "_reduct\\" + str(i) + ".png")
 
-        plt.imshow(r_CBCT, interpolation='nearest', cmap='bwr', origin='lower',vmin=-2000, vmax=2000)
-        plt.colorbar(shrink=.92)  # 设置一个颜色指引条
 
-        plt.xticks(())
-        plt.yticks(())
+def saveImage(data,savePath):
+    plt.imshow(data, interpolation='nearest', cmap='bwr', origin='lower', vmin=-2000, vmax=2000)
 
-        plt.show()
+    plt.colorbar(shrink=.92)
 
+    plt.xticks(())
+    plt.yticks(())
+    plt.savefig(savePath)
+    plt.close("all")
 
 
 rootPath = "D:\\test_data"
